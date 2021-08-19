2e5c117e78c1eae578a4d144f07828cea4abbd9a
navyCK <ckck81026@gmail.com>
2021-08-19 10:13:44 +0900

    코드 정리

--- startbootstrap-grayscale-master/dist/js/menu.js
+++ startbootstrap-grayscale-master/dist/js/menu.js
@@ -1,39 +1,18 @@
-$(document).ready(function(){
-
-/*
-$("선택자").hover(function(){
-마우스엔터상황
-},function(){
-마우스리브상황
-});
-*/
-$(".menu").hover(function(){
-	$(this).find(".sub").stop().slideDown(500);
-	$(this).find(".main").addClass("on");
-
-
-},function(){
-	$(this).find(".sub").stop().slideUp(500);
-	$(this).find(".main").removeClass("on");
-});
-
-
-});/*ready end*/
-
-jQuery(document).ready(function() {
-    var offset = 220;
-    var duration = 500;
-    jQuery(window).scroll(function() {
-        if (jQuery(this).scrollTop() > offset) {
-            jQuery('.back_to_top').fadeIn(duration);
-        } else {
-            jQuery('.back_to_top').fadeOut(duration);
+$(document).ready(function () {
+    $(".menu").hover(
+        function () {
+            $(this).find(".sub").stop().slideDown(500), $(this).find(".main").addClass("on");
+        },
+        function () {
+            $(this).find(".sub").stop().slideUp(500), $(this).find(".main").removeClass("on");
         }
+    );
+}),
+    jQuery(document).ready(function () {
+        jQuery(window).scroll(function () {
+            jQuery(this).scrollTop() > 220 ? jQuery(".back_to_top").fadeIn(500) : jQuery(".back_to_top").fadeOut(500);
+        }),
+            jQuery(".back_to_top").click(function (n) {
+                return n.preventDefault(), jQuery("html, body").animate({ scrollTop: 0 }, 500), !1;
+            });
     });
-
-    jQuery('.back_to_top').click(function(event) {
-        event.preventDefault();
-        jQuery('html, body').animate({scrollTop: 0}, duration);
-        return false;
-    })
-});
--- startbootstrap-grayscale-master/dist/js/scroll.js
+++ startbootstrap-grayscale-master/dist/js/scroll.js
@@ -1,21 +1,9 @@
-$( document ).ready( function() {
-
-    $( window ).scroll( function() {
-
-        if ( $( this ).scrollTop() > 10 ) {
-
-            $('#mainNav').fadeOut();
-
-
-        } 
-
-        else { 
-
-            $('#mainNav').fadeIn(); 
-
-
-        } 
-
-    });
-
-});
+$(window).on("mousewheel", function (o) {
+  o.originalEvent.wheelDelta > 0 ? ($("#mainNav").fadeIn(), $(".btn_gotop").fadeOut()) : ($("#mainNav").fadeOut(), $(this).scrollTop() > 1e3 && $(".btn_gotop").fadeIn());
+}),
+  $(window).scroll(function () {
+      $(this).scrollTop() < 10 && $(".btn_gotop").fadeOut();
+  }),
+  $(".btn_gotop").click(function () {
+      return $("html, body").animate({ scrollTop: 0 }, 400), !1;
+  });
